41d82559571c4e459eeb45ac22115f6939a14d78
Lighwingston <137218982+Lighwingston@users.noreply.github.com>
2026-05-19 18:40:08 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const FEATURES = [
     { name: 'AvatarVision', path: './features/avatar-vision.js' },
     { name: 'CustomVectors', path: './features/custom-vectors.js' },
     { name: 'AutoModelFetch', path: './features/auto-model-fetch.js' },
-    { name: 'FreeModelProfile', path: './features/freemodel-profile.js' },
+    { name: 'FreeModelSource', path: './features/freemodel-source.js' },
     // { name: 'NextFeature', path: './features/next-feature.js' },
 ];
 
